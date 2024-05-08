97a742fd6a17675de711ecd60e2faf9ede4e0f2e
ernur torekul <ernurtorekul@gmail.com>
2024-05-08 15:22:42 +0500

    finished with the sidebar except for design

---
+++ src/pages/Section2.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Section2() {
+  return (
+    <div className="h-screen w-full bg-blue-300 h-full flex ">
+      <h2 className="">Sectiondasdsadsadsadsadasdsadsadsadasdsa 22222</h2>
+    </div>
+  );
+}
+
+export default Section2;
---
+++ src/pages/Section3.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Section3() {
+  return (
+    <div className="h-screen w-full bg-blue-300 h-full flex ">
+      <h2 className="">Sectiondasdsadsadsadsadasdsadsadsadasdsa 33333</h2>
+    </div>
+  );
+}
+
+export default Section3;
